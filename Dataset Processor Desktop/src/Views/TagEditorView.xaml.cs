134cf81cd3bb03bb30703767f08685186b33e680
Leonardo <leonardo.fernandes98@hotmail.com>
2023-04-07 18:32:31 -0300

    fix: Off load the search functionality to a secondary thread for better GUI experience.

--- Dataset Processor Desktop/src/Views/TagEditorView.xaml.cs
+++ Dataset Processor Desktop/src/Views/TagEditorView.xaml.cs
@@ -2,36 +2,25 @@ using Dataset_Processor_Desktop.src.ViewModel;
 
 using SmartData.Lib.Interfaces;
 
-using System.ComponentModel;
 using System.Text.RegularExpressions;
 
 namespace Dataset_Processor_Desktop.src.Views;
 
-public partial class TagEditorView : ContentView, INotifyPropertyChanged
+public partial class TagEditorView : ContentView
 {
     private readonly IFileManipulatorService _fileManipulatorService;
     private readonly IImageProcessorService _imageProcessorService;
 
     private TagEditorViewModel _viewModel;
 
-    private Color _highlightBackgroundColor = new Color(255, 179, 71, 153);
-    private Color _transparentColor = new Color(0, 0, 0, 0);
-    private Color _textColor = new Color(255, 255, 255);
+    private Color _highlightBackgroundColor = Color.FromRgba(255, 179, 71, 153);
+    private Color _transparentColor = Color.FromRgba(0, 0, 0, 0);
+    private Color _textColor = Color.FromRgb(255, 255, 255);
 
     private FormattedString _labelFormattedString;
 
-    private CancellationTokenSource _cancellationTokenSource;
-    private TimeSpan _waitTimeInMilliseconds = new TimeSpan(0, 0, 2);
-
-    public FormattedString LabelFormattedString
-    {
-        get => _labelFormattedString;
-        set
-        {
-            _labelFormattedString = value;
-            OnPropertyChanged(nameof(LabelFormattedString));
-        }
-    }
+    private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+    private TimeSpan _waitTimeInMilliseconds = TimeSpan.FromSeconds(1);
 
     public TagEditorView(IFileManipulatorService fileManipulatorService, IImageProcessorService imageProcessorService)
     {
@@ -43,65 +32,64 @@ public partial class TagEditorView : ContentView, INotifyPropertyChanged
         _viewModel = new TagEditorViewModel(_fileManipulatorService, _imageProcessorService);
         BindingContext = _viewModel;
 
-        EditorHighlight.TextChanged += (sender, args) => DebounceOnTextChanged(() => OnTextChanged(sender, args));
-        EditorTags.TextChanged += (sender, args) => DebounceOnTextChanged(() => OnTextChanged(sender, args));
+        EditorHighlight.TextChanged += async (sender, args) => await DebounceOnTextChangedAsync(() => OnTextChanged(sender, args));
+        EditorTags.TextChanged += async (sender, args) => await DebounceOnTextChangedAsync(() => OnTextChanged(sender, args));
 
-        LabelFormattedString = new FormattedString();
+        _labelFormattedString = new FormattedString();
     }
 
-    private async void DebounceOnTextChanged(Action action)
+    private async Task DebounceOnTextChangedAsync(Action action)
     {
-        _cancellationTokenSource?.Cancel();
+        _cancellationTokenSource.Cancel();
+        _cancellationTokenSource.Dispose();
         _cancellationTokenSource = new CancellationTokenSource();
 
         try
         {
             await Task.Delay(_waitTimeInMilliseconds, _cancellationTokenSource.Token);
-            action.Invoke();
+            if (!_cancellationTokenSource.IsCancellationRequested)
+            {
+                action.Invoke();
+            }
         }
         catch (TaskCanceledException)
         {
             return;
         }
-        finally
-        {
-            _cancellationTokenSource?.Dispose();
-            _cancellationTokenSource = null;
-        }
     }
 
-    private void OnTextChanged(object sender, TextChangedEventArgs e)
+    private async void OnTextChanged(object sender, TextChangedEventArgs e)
     {
+        if (string.IsNullOrEmpty(EditorHighlight.Text))
+        {
+            _labelFormattedString.Spans.Clear();
+            LabelFormatted.FormattedText = _labelFormattedString;
+            return;
+        }
+        if (string.IsNullOrEmpty(_viewModel.WordsToHighlight))
+        {
+            return;
+        }
+        if (string.IsNullOrEmpty(EditorTags.Text))
+        {
+            return;
+        }
+
+        string editorText = EditorTags.Text;
+        string[] parts = editorText.Replace(", ", ",").Split(",");
+        string[] wordsToHighlighSplit = _viewModel.WordsToHighlight.Replace(", ", ",").Split(",");
+
+        _labelFormattedString = await Task.Run(() => UpdateFormattedString(parts, wordsToHighlighSplit));
+
         MainThread.BeginInvokeOnMainThread(() =>
         {
-            if (string.IsNullOrEmpty(EditorHighlight.Text))
-            {
-                LabelFormattedString.Spans.Clear();
-                LabelFormatted.FormattedText = LabelFormattedString;
-                return;
-            }
-            if (string.IsNullOrEmpty(_viewModel.WordsToHighlight))
-            {
-                return;
-            }
-            if (string.IsNullOrEmpty(EditorTags.Text))
-            {
-                return;
-            }
-
-            string editorText = EditorTags.Text;
-            string[] parts = editorText.Replace(", ", ",").Split(",");
-            string[] wordsToHighlighSplit = _viewModel.WordsToHighlight.Replace(", ", ",").Split(",");
-
-            UpdateFormattedString(parts, wordsToHighlighSplit);
-
-            LabelFormatted.FormattedText = LabelFormattedString;
+            LabelFormatted.FormattedText = _labelFormattedString;
         });
     }
 
-    private void UpdateFormattedString(string[] parts, string[] wordsToHighlighSplit)
+    private FormattedString UpdateFormattedString(string[] parts, string[] wordsToHighlighSplit)
     {
-        LabelFormattedString.Spans.Clear();
+        FormattedString formattedString = new FormattedString();
 
         string regexSearchPattern = $@"\b({string.Join("|", wordsToHighlighSplit)})\b";
         Regex regex = new Regex(regexSearchPattern, RegexOptions.IgnoreCase);
@@ -127,7 +115,9 @@ public partial class TagEditorView : ContentView, INotifyPropertyChanged
             }
 
             span.TextColor = _textColor;
-            LabelFormattedString.Spans.Add(span);
+            formattedString.Spans.Add(span);
         }
+
+        return formattedString;
     }
 }
