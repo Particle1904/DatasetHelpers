c1283d40bb344b13526641b48cebafab9bebc31e
Leonardo Fernandes <leonardo.fernandes98@hotmail.com>
2023-10-10 06:47:48 -0300

    chore: Setup MainView initial Layout

--- DatasetProcessor.Desktop/Program.cs
+++ DatasetProcessor.Desktop/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Avalonia;
+using Avalonia.Svg.Skia;
 
-using Avalonia;
+using System;
 
 namespace DatasetProcessor.Desktop;
 
@@ -15,9 +16,13 @@ class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        GC.KeepAlive(typeof(SvgImageExtension).Assembly);
+        GC.KeepAlive(typeof(Avalonia.Svg.Skia.Svg).Assembly);
+
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
-
+    }
 }
