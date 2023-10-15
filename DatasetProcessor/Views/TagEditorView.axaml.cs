7d0d1ecbc0615205106d8f9245ecbe48bfaa0563
Leonardo Fernandes <leonardo.fernandes98@hotmail.com>
2023-10-15 06:26:28 -0300

    feat: Add text highligting inside the Tags Editor

--- DatasetProcessor/Views/TagEditorView.axaml.cs
+++ DatasetProcessor/Views/TagEditorView.axaml.cs
@@ -1,7 +1,13 @@
 using Avalonia.Controls;
 using Avalonia.Media;
 
+using AvaloniaEdit.Document;
+
+using DatasetProcessor.src.Classes;
+using DatasetProcessor.ViewModels;
+
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,21 +16,54 @@ namespace DatasetProcessor.Views
 {
     public partial class TagEditorView : UserControl
     {
-        private Color _highlightBackgroundColor = Color.FromArgb(153, 255, 179, 71);
-        private Color _transparentColor = Color.FromArgb(0, 0, 0, 0);
-        private Color _textColor = Color.FromRgb(255, 255, 255);
-
-        private FormattedText _labelFormattedString;
+        private Color _highlightTextColor = Color.FromArgb(255, 255, 179, 71);
 
         private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-        private TimeSpan _highlightUpdateDelay = TimeSpan.FromSeconds(3);
+        private TimeSpan _highlightUpdateDelay = TimeSpan.FromSeconds(0.75);
+
+        private TagEditorViewModel _viewModel;
+
+        private TextDocument _textDocument;
+
+        SolidColorBrush _transparentForeground;
+        SolidColorBrush _highlightForeground;
 
         public TagEditorView()
         {
             InitializeComponent();
 
-            //EditorHighlight.TextChanged += async (sender, args) => await DebounceOnTextChangedAsync(() => OnTextChanged(sender, args));
-            //EditorTags.TextChanged += async (sender, args) => await DebounceOnTextChangedAsync(() => OnTextChanged(sender, args));
+            EditorHighlight.TextChanged += async (sender, args) => await DebounceOnTextChangedAsync(() => OnEditorHighlightTextChanged(sender, args));
+            EditorTags.TextChanged += OnTextChanged;
+            _textDocument = new TextDocument();
+            EditorTags.Document = _textDocument;
+        }
+
+        private void OnEditorHighlightTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_viewModel != null)
+            {
+                string[] tagsToHighlight = EditorHighlight.Text.Replace(", ", ",").Split(",");
+
+                EditorTags.SyntaxHighlighting = new TagsSyntaxHighlight(_highlightTextColor, tagsToHighlight);
+            }
+        }
+
+        private void OnTextChanged(object sender, EventArgs args)
+        {
+            if (_viewModel != null)
+            {
+                _viewModel.PropertyChanged -= OnTagsPropertyChanged;
+                _viewModel.CurrentImageTags = EditorTags.Text;
+                _viewModel.PropertyChanged += OnTagsPropertyChanged;
+            }
+        }
+
+        protected override void OnDataContextChanged(EventArgs e)
+        {
+            _viewModel = (TagEditorViewModel)DataContext;
+            _viewModel.PropertyChanged += OnTagsPropertyChanged;
+
+            base.OnDataContextChanged(e);
         }
 
         private async Task DebounceOnTextChangedAsync(Action action)
@@ -44,6 +83,14 @@ namespace DatasetProcessor.Views
             catch (TaskCanceledException)
             {
                 Debug.WriteLine("Task canceled.");
+            }
+        }
+
+        private void OnTagsPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_viewModel.CurrentImageTags))
+            {
+                EditorTags.Text = _viewModel.CurrentImageTags;
             }
         }
     }
---
+++ DatasetProcessor/src/Classes/TagsSyntaxHighlight.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia.Media;
+
+using AvaloniaEdit.Highlighting;
+
+using SmartData.Lib.Helpers;
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DatasetProcessor.src.Classes
+{
+    public class TagsSyntaxHighlight : IHighlightingDefinition
+    {
+        public string Name => "TagsSyntaxHighlight";
+
+        public HighlightingRuleSet MainRuleSet { get; }
+
+        public TagsSyntaxHighlight(Color foregroundColor, string[] tags)
+        {
+            MainRuleSet = new HighlightingRuleSet();
+
+            for (int i = 0; i < tags.Length; i++)
+            {
+                if (tags[i].Length == 0 || string.IsNullOrEmpty(tags[i]))
+                {
+                    continue;
+                }
+
+                HighlightingRule customWordRule = new HighlightingRule()
+                {
+                    Color = new HighlightingColor { Foreground = new SimpleHighlightingBrush(foregroundColor) },
+                    Regex = new Regex(@$"\b({Regex.Escape(tags[i])})\b", RegexOptions.IgnoreCase, Utilities.RegexTimeout)
+                };
+
+                MainRuleSet.Rules.Add(customWordRule);
+            }
+        }
+
+        public IEnumerable<HighlightingColor> NamedHighlightingColors => null;
+
+        public IDictionary<string, string> Properties => null;
+
+        public HighlightingColor GetNamedColor(string name) => null;
+        public HighlightingRuleSet GetNamedRuleSet(string name) => null;
+    }
+}
