47a2dd67aa426e1ccce9df0cd71fe72e205845d5
Leonardo Fernandes <leonardo.fernandes98@hotmail.com>
2023-10-28 15:27:14 -0300

    feat: Add more resolutions, all multiples of 32

--- SmartData.Lib/Enums/SupportedDimensions.cs
+++ SmartData.Lib/Enums/SupportedDimensions.cs
@@ -2,12 +2,24 @@
 
 namespace SmartData.Lib.Enums
 {
-    public enum SupportedDimensions
+    public enum SupportedDimensions : ushort
     {
         Resolution512x512 = 512,
+        Resolution544x544 = 544,
         Resolution576x576 = 576,
+        Resolution608x608 = 608,
         Resolution640x640 = 640,
+        Resolution672x672 = 672,
+        Resolution704x704 = 704,
+        Resolution736x736 = 736,
         Resolution768x768 = 768,
+        Resolution800x800 = 800,
+        Resolution832x832 = 832,
+        Resolution864x864 = 864,
+        Resolution896x896 = 896,
+        Resolution897x897 = 928,
+        Resolution960x960 = 960,
+        Resolution992x992 = 992,
         Resolution1024x1024 = 1024
     }
 }
